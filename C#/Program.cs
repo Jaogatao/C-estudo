234949270736dcac411b571c2d051c4960eeb451
João Antônio <105800868+Jaogatao@users.noreply.github.com>
2024-06-01 23:11:15 -0300

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Talvez uma nova experiência");
+        }
+    }
+}
