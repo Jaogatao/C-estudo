b8b72efb07fc50ae666d01f59ef8a440aea9d4d8
João Antônio <105800868+Jaogatao@users.noreply.github.com>
2024-06-01 23:29:04 -0300

    Add files via upload

---
+++ Produto C#/Produto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Produto_C_
+{
+    public class Produto
+    {
+        public string 
+    }
+}
